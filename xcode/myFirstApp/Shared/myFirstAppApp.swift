b2642d8114f0e6d02f0f433268d7e0c815840c4b
joe-klingaman <joe.klingaman@theanimaltraders.com>
2022-06-07 00:13:33 -0500

    configured both iOS and Android

---
+++ xcode/myFirstApp/Shared/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  Shared
+//
+//  Created by Joe Klingaman on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
